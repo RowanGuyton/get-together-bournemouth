35bd88b3fd1f99ac7a4189f3939b4c347132e0d0
Rowan Guyton <rlguyton91@gmail.com>
2024-09-03 22:50:51 +0100

    Adds initial Event migration

---
+++ database/migrations/2024_09_03_210534_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->dateTime('date');
+            $table->string('description')->nullable();
+            $table->string('event_url');
+            // We don't always need a map URL if we know where we're going
+            $table->string('maps_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
