e84e2a7974bbf87f5d8e72e5ccb4938fcb97f350
Rowan Guyton <rlguyton91@gmail.com>
2024-09-04 19:13:18 +0100

    Adds nav link to event creation page

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('new-event')} active={route().current('new-event')}>
+                                    New Event
+                                </NavLink>
                             </div>
                         </div>
 
