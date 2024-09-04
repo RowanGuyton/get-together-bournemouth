68b0e1cd56d235859a3d4fac0696a3d8edbeeea5
Rowan Guyton <rlguyton91@gmail.com>
2024-09-04 19:12:03 +0100

    Adds controller logic for creating events

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use Illuminate\Support\Facades\Auth;
 
 class EventsController extends Controller
 {
@@ -18,6 +19,23 @@ class EventsController extends Controller
     }
     // TODO: Implement creating new events
     public function create(Request $request) {
+        $userId = Auth::user()->id;
 
+        $event = new Event();
+
+        $event->user_id = $userId;
+        $event->name= $request->input('name');
+        $event->date = $request->input('date');
+        $event->description = $request->input('description');
+        $event->event_url = $request->input('event_url');
+        $event->maps_url = $request->input('maps_url') ?? null;
+
+        $result = $event->save();
+
+        if (!$result) {
+            return response()->json('error', 500);
+        }
+
+        return response()->json('success');
     }
 }
